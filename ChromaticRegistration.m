e377e178c15107c4bac70a0365b377c65f7465e9
Jian Tay <jian.tay@colorado.edu>
2023-04-24 14:50:20 -0600

    Started a new class

---
+++ ChromaticRegistration.m
@@ -0,0 +1,36 @@
+classdef ChromaticRegistration
+    %CHROMATICREGISTRATION  Calculate and correct chromatic aberrations
+    %
+    %  CR = CHROMATICREGISTRATION creates a new CHROMATICREGISTRATION
+    %  object. This object can be used to calculate a correction for
+    %  chromatic aberration for microscopy.
+    %
+    %  To obtain the correction, you need to image fiducial markers (i.e.,
+    %  either an image of multi-color beads or dots). The object then
+    %  calculates a displacement matrix.
+
+    properties (SetAccess = private)
+
+        
+
+    end
+    
+    
+    methods
+
+        function obj = calculateCorrection(obj, filepath)
+
+
+        end
+        
+        function obj = 
+
+        end
+
+    
+    
+    end
+
+
+
+end
